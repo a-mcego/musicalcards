fda21c55f7b2ae61638968e8b0074ff2f5d2c677
Angela McEgo <eve.loikkanen@gmail.com>
2024-05-05 01:06:20 +0300

    Beautify gdscript code

--- godot/mainscript.gd
+++ godot/mainscript.gd
@@ -3,7 +3,7 @@ extends Node3D
 var mouse_position:Vector3
 
 var textures:Dictionary = {} #only use through LoadTexture
-func LoadTexture(name):
+func LoadTexture(name) -> ImageTexture:
 	if !textures.has(name):
 		var image = Image.new()
 		image.load(name)
@@ -12,6 +12,7 @@ func LoadTexture(name):
 
 class UIElement:
 	var name:String
+	var id:int
 	var texture:ImageTexture
 	var draggable:bool = false
 	var dragging:bool = false
@@ -19,8 +20,9 @@ class UIElement:
 	var size:Vector2 = Vector2(15,20)
 	var mesh_instance:MeshInstance3D
 	
-	func _init(name, texture:ImageTexture, center, size):
+	func _init(name:String, id:int, texture:ImageTexture, center:Vector2, size:Vector2) -> void:
 		self.name = name
+		self.id = id
 		self.texture = texture
 		self.size = size
 		self.mesh_instance = MeshInstance3D.new()
@@ -29,25 +31,27 @@ class UIElement:
 		self.mesh_instance.mesh = mesh
 		
 		var material = StandardMaterial3D.new()
-		material.albedo_color = Color(3, 3, 3) # make it brighter with 3
+		material.albedo_color = Color(3, 3, 3, 1) # make it brighter with 3
 		material.albedo_texture = texture
 		material.transparency = true
 		self.mesh_instance.material_override = material
 		self.mesh_instance.translate(Vector3(center.x,center.y,-10))
 		self.mesh_instance.rotate_x(1.57079632679)
 	
-	func SetTexture(texture:ImageTexture):
+	func SetTexture(texture:ImageTexture) -> void:
 		self.mesh_instance.material_override.albedo_texture = texture
 		
-	func Enable(value:bool):
-		if value:
-			self.mesh_instance.material_override.albedo_color = Color(3,3,3,1)
-		else:
-			self.mesh_instance.material_override.albedo_color = Color(3,3,3,0)
-
+	func Enable(value:bool) -> void:
+		self.mesh_instance.material_override.albedo_color.a = 1.0 if value else 0.0
+			
+	func GetRect() -> Rect2:
+		var meshpos3d = self.mesh_instance.global_transform.origin
+		var meshpos2d = Vector2(meshpos3d.x-self.size.x*0.5, meshpos3d.y-self.size.y*0.5)
+		return Rect2(meshpos2d, self.size)
+		
 var uielements:Array = []
-
 var cardnames:Array = []
+
 func init_cardnames():
 	cardnames.clear()
 	cardnames.append("joker")
@@ -56,7 +60,6 @@ func init_cardnames():
 	for suit in suits:
 		for rank in ranks:
 			cardnames.append(rank + "_" + suit);
-	
 
 var current_cards:Array = [null, null, null, null, null]
 var numbers:Array = []
@@ -67,52 +70,38 @@ var game_strings = {
 	"finished":"Congrats! Or not."
 }
 
-
-var locked:Array = [false,false,false,false,false]
+var locked_cards:Array = [false,false,false,false,false]
 
 func deal():
 	if gamestate == "start" or gamestate == "finished":
-		locked = [false,false,false,false,false]
+		locked_cards = [false,false,false,false,false]
 		numbers.clear()
 		for n in cardnames.size():
 			numbers.append(n)
 		numbers.shuffle()
 		for n in current_cards.size():
 			current_cards[n] = numbers[n]
-			var texname = "res://cards/back.png"
-			if current_cards[n] != null:
-				texname = "res://cards/" + cardnames[current_cards[n]] + ".png"
+			var texname = "res://cards/" + cardnames[current_cards[n]] + ".png"
 			uielements[n].SetTexture(LoadTexture(texname))
 		gamestate = "firstdeal"
 	elif gamestate == "firstdeal":
 		for n in current_cards.size():
-			if !locked[n]:
+			if !locked_cards[n]:
 				current_cards[n] = numbers[n+5]
-				var texname = "res://cards/back.png"
-				if current_cards[n] != null:
-					texname = "res://cards/" + cardnames[current_cards[n]] + ".png"
+				var texname = "res://cards/" + cardnames[current_cards[n]] + ".png"
 				uielements[n].SetTexture(LoadTexture(texname))
 		gamestate = "finished"
 	
-	
-func Pressed(name:String):
+func Pressed(name:String, id:int):
 	if name == "deal":
 		deal()
 	
 	if gamestate == "firstdeal":
-		if name == "card0":
-			locked[0] = !locked[0]
-		if name == "card1":
-			locked[1] = !locked[1]
-		if name == "card2":
-			locked[2] = !locked[2]
-		if name == "card3":
-			locked[3] = !locked[3]
-		if name == "card4":
-			locked[4] = !locked[4]
+		if name == "card":
+			locked_cards[id] = !locked_cards[id]
 
-	for n in locked.size():
-		uielements[n+5].Enable(locked[n])
+	for n in locked_cards.size():
+		uielements[n+5].Enable(locked_cards[n])
 		
 	get_child(1).text = game_strings[gamestate] #Label node
 		
@@ -121,9 +110,9 @@ func _ready():
 	
 	for n in 5:
 		var texname = "res://cards/back.png"
-		
 		var card:UIElement = UIElement.new(
-			"card"+str(n),
+			"card",
+			n,
 			LoadTexture(texname), 
 			Vector2(32*(n-2),0),
 			Vector2(30,40)
@@ -133,9 +122,9 @@ func _ready():
 
 	for n in 5:
 		var texname = "res://cards/kept.png"
-		
 		var card:UIElement = UIElement.new(
-			"kept"+str(n),
+			"kept",
+			n,
 			LoadTexture(texname), 
 			Vector2(32*(n-2),25),
 			Vector2(30,8)
@@ -146,6 +135,7 @@ func _ready():
 	
 	var dealButton = UIElement.new(
 		"deal",
+		0,
 		LoadTexture("res://cards/deal.png"),
 		Vector2(64,-32),
 		Vector2(24,8)
@@ -154,26 +144,20 @@ func _ready():
 	uielements.append(dealButton)
 
 func _input(event):
-	for card in uielements:
+	for uielement in uielements:
 		if event is InputEventMouseButton:
 			mouse_position = get_viewport().get_camera_3d().project_ray_origin(event.position)
 			if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
 				if event.pressed:
-					var meshpos3d = card.mesh_instance.global_transform.origin
-					var meshpos2d = Vector2(meshpos3d.x-card.size.x*0.5, meshpos3d.y-card.size.y*0.5)
-					
-					var currrect:Rect2 = Rect2(meshpos2d, card.size)
+					var currrect:Rect2 = uielement.GetRect()
 					if currrect.has_point(Vector2(mouse_position.x, mouse_position.y)):
-						Pressed(card.name)
-						if card.draggable:
-							card.dragging = true
-							card.drag_offset = card.mesh_instance.global_transform.origin - mouse_position
+						Pressed(uielement.name, uielement.id)
+						if uielement.draggable:
+							uielement.dragging = true
+							uielement.drag_offset = uielement.mesh_instance.global_transform.origin - mouse_position
 				else:
-					card.dragging = false
+					uielement.dragging = false
 
-		elif event is InputEventMouseMotion and card.dragging:
+		elif event is InputEventMouseMotion and uielement.dragging:
 			mouse_position = get_viewport().get_camera_3d().project_ray_origin(event.position)
-			card.mesh_instance.global_transform.origin = mouse_position + card.drag_offset
-
-func _process(delta):
-	pass
+			uielement.mesh_instance.global_transform.origin = mouse_position + uielement.drag_offset
